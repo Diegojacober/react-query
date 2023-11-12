6b9470d401dcc4c65301946d9211f7bb588111b0
Diego Alencar Jacober <diegoalencar.jacober@gmail.ocm>
2023-11-12 16:41:57 -0300

    :construction: pre-fetching data

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,25 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PostDetail } from "./PostDetail";
 import { Post } from "./queries/post/types";
 import api from "./services/api";
-import { useQuery } from "@tanstack/react-query";
+import {
+  useQuery,
+  QueryFunctionContext,
+  useQueryClient,
+  keepPreviousData,
+} from "@tanstack/react-query";
 const maxPostPage = 10;
 
-async function fetchPosts() {
-  const { data } = await api.get<Post[]>(`/posts?_limit=10&_page=0`);
+async function fetchPosts(ctx: QueryFunctionContext) {
+  const [, pageNum] = ctx.queryKey;
+  const { data } = await api.get<Post[]>(`/posts?_limit=10&_page=${pageNum}`);
 
   return data;
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [selectedPost, setSelectedPost] = useState<Post | null>(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: fetchPosts,
+      });
+    }
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
-  const { data, isError, error, isLoading } = useQuery({ queryKey: ["posts"], queryFn: fetchPosts, staleTime: 2000 });
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["posts", currentPage],
+    queryFn: fetchPosts,
+    staleTime: 2000,
+    placeholderData: keepPreviousData,
+  });
   if (isLoading) return <h3>Loading...</h3>;
 
-  if (isError) return <h3>Oops, somenthing went wrong <p>{error.message}</p></h3>;
+  if (isError)
+    return (
+      <h3>
+        Oops, somenthing went wrong <p>{error.message}</p>
+      </h3>
+    );
 
   return (
     <>
@@ -35,11 +63,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previouesValue) => previouesValue - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previouesValue) => previouesValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
