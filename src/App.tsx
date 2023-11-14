f74bc19b48de1d62e666c1e7a54acb96b8642d6e
Diego Alencar Jacober <diegoalencar.jacober@gmail.ocm>
2023-11-13 21:06:07 -0300

    :construction: write useInfiniteQuery call

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import "./App.css";
 import { InfinitePeople } from "./components/people/InifinitePeople";
 import { InfiniteSpecies } from "./components/species/InfiniteSpecies";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryCLient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryCLient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <>
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/people/InifinitePeople.tsx
+++ src/components/people/InifinitePeople.tsx
@@ -1,10 +1,43 @@
-import InfiniteScroll from 'react-infinite-scroller';
+import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Person } from "./Person";
-import api from '../../services/api';
+import api from "../../services/api";
 
-const { data } = api.get<PersonProps>("/people/")
+const fetchPosts = async ({ pageParam = 1 }) => {
+  const data = await api.get<PersonProps>(`/people/`, {
+    params: { page: pageParam },
+  });
+  return data;
+};
+
+interface Page {
+  data: any;
+  previousCursor?: number;
+  nextCursor?: number;
+}
 
 export function InfinitePeople() {
+  const {
+    data, // data accumulated across all pages: InfiniteQueryData<Page>
+    error, // any Error that has occurred
+    fetchNextPage, // Function to fetch the next page
+    fetchPreviousPage,
+    hasNextPage, // Boolean that indicates if the next page is available
+    hasPreviousPage,
+    isError, // If an Error has occurred
+    isFetching, // If the query function is running
+    isFetchingNextPage,
+    isFetchingPreviousPage,
+    status, // 'error' | 'loading' | 'success' - indicates state of this hook
+  } = useInfiniteQuery<Page, Error>({
+    queryKey: ["data"],
+    queryFn: fetchPosts,
+    getLastPageParam: (firstPage: Page, pages: Page[]) =>
+      firstPage.previousCursor,
+    getNextPageParam: (lastPage: Page, pages: Page[]) => lastPage.nextCursor,
+  });
+
+  console.log(data)
   // TODO: get data for InfiniteScroll via React Query
   return <InfiniteScroll />;
 }
